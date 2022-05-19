a2b29a2ddfc76203ff15c7a7adf64417b3bbb821
vrb9999 <vbiradar9999@gmail.com>
2022-05-19 08:58:45 +0530

    UC-1 Day-19 Validate FirstName

---
+++ FirstName/FirstName/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validate userRegistration = new Validate();
+            Console.WriteLine("Enter a First Name");
+            string FirstName = Console.ReadLine();
+            userRegistration.validfirstname(FirstName);
+        }
+    }
+}
---
+++ FirstName/FirstName/Validate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FirstName
+{
+    class Validate
+    {
+        public static string RegexRules = "^[A-Z][a-z]{2,}$";
+        public Regex FirstNameRegex = new Regex(RegexRules);
+        public void validfirstname(string FirstName)
+        {
+            if (Regex.IsMatch(FirstName, RegexRules))
+            {
+                Console.WriteLine("Valid");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
