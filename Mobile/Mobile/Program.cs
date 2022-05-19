92bfd7c8ce2db304a154a094025fd82072658c5c
vrb9999 <vbiradar9999@gmail.com>
2022-05-19 09:10:03 +0530

    UC-4 Day-19 Validate Mobile

---
+++ Mobile/Mobile/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Mobile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validate userRegistration = new Validate();
+            Console.WriteLine("Enter Mobile number :");
+            string mobile = Console.ReadLine();
+            userRegistration.validmobile(mobile);
+        }
+    }
+}
---
+++ Mobile/Mobile/Validate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Mobile
+{
+    class Validate
+    {
+        public static string RegexRules = "^[0-9]{2}[ ]{1}[0-9]{10}$";
+        public Regex MobileRegex = new Regex(RegexRules);
+        public void validmobile(string Mobile)
+        {
+            if (Regex.IsMatch(Mobile, RegexRules))
+            {
+                Console.WriteLine("Valid");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
