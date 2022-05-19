76b6b911c63faa50ce6b40c09dc85e07b6ae09e7
vrb9999 <vbiradar9999@gmail.com>
2022-05-19 09:30:12 +0530

    UC-6 Day-19 Password should have atleast 1 uppercase

---
+++ PR2_UpperCase/PR2_UpperCase/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PR2_UpperCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validate userRegistration = new Validate();
+            Console.WriteLine("Enter a Password");
+            string Password = Console.ReadLine();
+            userRegistration.validpassword(Password);
+        }
+    }
+}
---
+++ PR2_UpperCase/PR2_UpperCase/Validate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PR2_UpperCase
+{
+    class Validate
+    {
+        public static string RegexRules = "(?=.*[A-Z])[a-zA-Z0-9]{8,}$";
+        public Regex PasswordRegex = new Regex(RegexRules);
+        public void validpassword(string Password)
+        {
+            if (Regex.IsMatch(Password, RegexRules))
+            {
+                Console.WriteLine("Valid");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
