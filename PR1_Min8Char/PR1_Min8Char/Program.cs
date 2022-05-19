f14d6164f7c839ec3338dfe8e833ea7f15e283ad
vrb9999 <vbiradar9999@gmail.com>
2022-05-19 09:28:21 +0530

    UC-5 Day-19 Password should contain min 8 characters

---
+++ PR1_Min8Char/PR1_Min8Char/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PR1_Min8Char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validate userRegistration = new Validate();
+            Console.WriteLine("Enter a Password");
+            string Password = Console.ReadLine();
+            userRegistration.validpassword(Password);
+        }
+    }
+}
---
+++ PR1_Min8Char/PR1_Min8Char/Validate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PR1_Min8Char
+{
+    class Validate
+    {
+        public static string RegexRules = "[a-zA-Z0-9]{8,}$";
+        public Regex PasswordRegex = new Regex(RegexRules);
+        public void validpassword(string Password)
+        {
+            if (Regex.IsMatch(Password, RegexRules))
+            {
+                Console.WriteLine("Valid");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
