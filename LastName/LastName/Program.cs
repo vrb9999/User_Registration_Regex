4bef2562f89b142b7f87d7a6127e36c298b6afd8
vrb9999 <vbiradar9999@gmail.com>
2022-05-19 09:03:18 +0530

    UC-2 Day-19 Validate LastName

---
+++ LastName/LastName/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LastName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validate userRegistration = new Validate();
+            Console.WriteLine("Enter a Last Name");
+            string LastName = Console.ReadLine();
+            userRegistration.validlastname(LastName);
+        }
+    }
+}
---
+++ LastName/LastName/Validate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LastName
+{
+    class Validate
+    {
+        public static string RegexRules = "^[A-Z][a-z]{2,}$";
+        public Regex LastNameRegex = new Regex(RegexRules);
+        public void validlastname(string LastName)
+        {
+            if (Regex.IsMatch(LastName, RegexRules))
+            {
+                Console.WriteLine("Valid");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
