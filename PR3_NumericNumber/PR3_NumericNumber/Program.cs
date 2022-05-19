af8aa39c5fa26579f65bc9c0589c58c1b2e9ae7d
vrb9999 <vbiradar9999@gmail.com>
2022-05-19 11:39:15 +0530

    UC-7 Day-19 Password should have atleast 1 numeric number

---
+++ PR3_NumericNumber/PR3_NumericNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PR3_NumericNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validate userRegistration = new Validate();
+            Console.WriteLine("Enter a Password");
+            string Password = Console.ReadLine();
+            userRegistration.validpassword(Password);
+        }
+    }
+}
