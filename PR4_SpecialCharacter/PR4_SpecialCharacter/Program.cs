e5b0cd1110edbc47f18acb442a74cc2fefc1d5b0
vrb9999 <vbiradar9999@gmail.com>
2022-05-19 11:45:02 +0530

    UC-8 Day-19 Password should have exactly one special character

---
+++ PR4_SpecialCharacter/PR4_SpecialCharacter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PR4_SpecialCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validate userRegistration = new Validate();
+            Console.WriteLine("Enter a Password");
+            string Password = Console.ReadLine();
+            userRegistration.validpassword(Password);
+        }
+    }
+}
---
+++ PR4_SpecialCharacter/PR4_SpecialCharacter/Validate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PR4_SpecialCharacter
+{
+    class Validate
+    {
+        public static string RegexRules = "^(?=.{8,})(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9]*[#!@$^&-][a-zA-Z0-9]*$";
+        public Regex PasswordRegex = new Regex(RegexRules);
+        public void validpassword(string Password)
+        {
+            if (Regex.IsMatch(Password, RegexRules))
+            {
+                Console.WriteLine("Valid");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
